5b79d64212828611c6d4b54ab0ee4efd9e89405c
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-05-05 21:21:35 -0700

    added colorpalette manipulation

--- bitmap-mosnter/index.js
+++ bitmap-mosnter/index.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const bitmapreader = require('./lib/readbitmap');
-const bitoobject = require('./lib/parser');
+const bitobject = require('./lib/parser');
+const fs = require('fs');
 
 //read bitmapfile
 bitmapreader.bitmapHeader('bitmap1.bmp', (err, data) => {
-  var newobject = new bitoobject.Buffobject(data);
+  var newobject = new bitobject.Buffobject(data);
   console.log(newobject);
+  newobject.colorpalette.fill(128);
+  fs.writeFile('./lulwat.bmp', data);
 });
 
 //write new bitmapfile
-bitmapreader.newbitmapfile(buffer, (err, data) => {
-
-});
+/*bitmapreader.newbitmapfile(brandnewbuffer, (err, data) => {
+  console.log(data);
+});*/
